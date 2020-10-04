98676dae38b24bd4647ee29de206f1499c02828a
batyrotich <eddahbaty98@gmail.com>
2020-10-04 18:06:38 +0300

    created a dynamic route for sources and sources/articles

--- app/views.py
+++ app/views.py
@@ -2,11 +2,19 @@ from app import app
 from flask import render_template
 
 
-
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
-    
+    title = 'Home - Top News  Highlight Website Online'
+    return render_template('index.html', title = title)
+
+
+
+@app.route('/articles/<id>')
+def news(id):
+    article = get_articles(id)
+    print(article)
+    return render_template('articles.html',article = article)
+
